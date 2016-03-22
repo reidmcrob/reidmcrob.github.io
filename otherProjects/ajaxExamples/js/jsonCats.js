15f4c20b726150e91670a2a854a894663180451e
reidmcrob <reidmcrob1@gmail.com>
2016-03-22 09:55:06 -0400

    hy

--- otherProjects/ajaxExamples/js/jsonCats.js
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -2,5 +2,36 @@ $(document).ready(function(){
 
   $.getJSON("jsonDatabase/cat.json",function(data){
     console.dir(data);
+    var html="";
+    $.each(data,function(index, item){
+    html += '<div class="col-md-4 cat">' +
+    '<div class= "catName"> ' +item.name+ '</div>'+
+    '<div class= "catType">' +item.type+ '</div>'+
+    '<div class= "catGender">' +item.gender+ '</div>'+
+    '<img src="'+item.image+'"/>';
+
+    //do some stuff
+    html+= '</div>'
+
+    })//each cat
+    @("#catData").append(html);
   })
 })
+/*
+//one per cat
+<div class="col-md-4 cat">
+<div class= "catName"></div>
+<div class= "catType"></div>
+<div class= "catGender"></div>
+<img src=""/>
+<div class="commentContainer">
+  <div class="renterName"></div>
+  <div class="renterLocation"></div>
+  <div class="renterStars"></div>
+  //5 stars, some full, some empty
+</div> //end starts
+</div> //end comments container
+</div> //ends cat
+
+
+*/
